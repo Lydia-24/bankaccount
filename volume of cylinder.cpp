a92322ddd84390b9ef0b45b8204e8d9252332020
Lydia-24 <lydiakamau3647@gmail.com>
2025-02-02 00:12:14 +0300

    first commit

---
+++ volume of cylinder.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+double pi=3.142;
+int main(){
+int radius,height;
+double volume;
+cout<<"enter the radius"<<endl;
+cin>>radius;
+cout<<"enter the height"<<endl;
+cin>>height;
+volume = pi*radius*radius*height;
+cout<<"volume,"<<volume<<endl;
+cin>>volume;
+return 0;
+}
